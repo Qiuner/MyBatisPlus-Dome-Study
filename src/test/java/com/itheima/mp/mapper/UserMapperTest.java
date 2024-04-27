669fa81b0cb32e565c41d74039b4576306bb467f
Qiu <wdst3635@163.com>
2024-04-28 07:53:12 +0800

    插件之分页插件用法

--- src/test/java/com/itheima/mp/mapper/UserMapperTest.java
+++ src/test/java/com/itheima/mp/mapper/UserMapperTest.java
@@ -3,12 +3,14 @@ package com.itheima.mp.mapper;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.conditions.update.UpdateWrapper;
 import com.itheima.mp.domain.po.User;
+import com.itheima.mp.domain.po.UserInfo;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 @SpringBootTest
 class UserMapperTest {
@@ -24,7 +26,8 @@ class UserMapperTest {
         user.setPassword("123");
         user.setPhone("18688990011");
         user.setBalance(200);
-        user.setInfo("{\"age\": 24, \"intro\": \"英文老师\", \"gender\": \"female\"}");
+        // user.setInfo("{\"age\": 24, \"intro\": \"英文老师\", \"gender\": \"female\"}");
+        user.setInfo(UserInfo.of(24,"英文老师","female"));
         user.setCreateTime(LocalDateTime.now());
         user.setUpdateTime(LocalDateTime.now());
         userMapper.insert(user);
--- src/test/java/com/itheima/mp/service/IUserServiceTest.java
+++ src/test/java/com/itheima/mp/service/IUserServiceTest.java
@@ -1,6 +1,8 @@
 package com.itheima.mp.service;
 
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.itheima.mp.domain.po.User;
+import com.itheima.mp.domain.po.UserInfo;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -49,7 +51,7 @@ class IUserServiceTest {
         user.setPassword("123");
         user.setPhone("" + (18688190000L + i));
         user.setBalance(2000);
-        user.setInfo("{\"age\": 24, \"intro\": \"英文老师\", \"gender\": \"female\"}");
+        user.setInfo(UserInfo.of(24,"英文老师","female"));
         user.setCreateTime(LocalDateTime.now());
         user.setUpdateTime(user.getCreateTime());
         return user;
@@ -64,7 +66,7 @@ class IUserServiceTest {
         user.setPassword("123");
         user.setPhone("18688990011");
         user.setBalance(200);
-        user.setInfo("{\"age\": 24, \"intro\": \"英文老师\", \"gender\": \"female\"}");
+        user.setInfo(UserInfo.of(24,"英文老师","female"));
         user.setCreateTime(LocalDateTime.now());
         user.setUpdateTime(LocalDateTime.now());
         userService.save(user);
@@ -74,5 +76,19 @@ class IUserServiceTest {
     void testQuery(){
         userService.listByIds(List.of(1L,2L,3L,4L))
                 .forEach(System.out::println);
+    }
+
+
+    @Test
+    void testPageQuery() {
+        // 1.分页查询，new Page()的两个参数分别是：页码、每页大小
+        Page<User> p = userService.page(new Page<>(2, 2));
+        // 2.总条数
+        System.out.println("total = " + p.getTotal());
+        // 3.总页数
+        System.out.println("pages = " + p.getPages());
+        // 4.数据
+        List<User> records = p.getRecords();
+        records.forEach(System.out::println);
     }
 }
